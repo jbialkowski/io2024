34dd08f8d8df5e83109ab73cf7d7623f853fd8d7
Jerzy Bialkowski <jb@mat.umk.pl>
2024-10-22 15:35:44 +0200

    korekta pliku naglowkowego

--- funkcje.h
+++ funkcje.h
@@ -2,11 +2,7 @@
 #define _FUNKCJE_H_
 
 #define INDEX_MIN 123456
-<<<<<<< HEAD
 #define INDEX_MAX 123458
-=======
-#define INDEX_MAX 123457
->>>>>>> b654a0fa875fbca1321e94b96d6a2a3546a36a2f
 
 
 int wypisz(int);
@@ -15,12 +11,12 @@ int wypisz_wszystkich();
 void wypisz_123456(void);
 const char *autor_123456(void);
 
-<<<<<<< HEAD
+
 void wypisz_123458(void);
 const char *autor_123458(void);
-=======
+
 void wypisz_123457(void);
 const char *autor_123457(void);
->>>>>>> b654a0fa875fbca1321e94b96d6a2a3546a36a2f
+
 
 #endif
